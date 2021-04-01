b542fb2ca7dbf4efd91048fb2e704e95f0270d9f
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-04-02 00:31:57 +0100

    first commit

---
+++ app/Http/Controllers/UniController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\University;
+
+use Illuminate\Http\Request;
+
+class UniController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return University::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'type' => 'required',
+            'location' => 'required'
+        ]);
+        return University::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return University::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $university = University::find($id);
+        $university->update($request->all());
+        return $university;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if($university = University::destroy($id)){
+            $message = array('message' => 'University has been removed');
+            return $message;
+        }
+    }
+
+    /**
+     * Search resource from storage.
+     *
+     * @param  str  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return $university = University::where('name', 'like', '%'.$name.'%')->get();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\UniController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Public Route
+// Route::resource('universities', UniController::class);
+Route::post('/register', [UserController::class, 'register']);
+Route::get('/universities', [UniController::class, 'index']);
+Route::get('/universities/{id}', [UniController::class, 'show']);
+Route::get('/universities/search/{name}', [UniController::class, 'search']);
+
+
+// Protected Route
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/universities', [UniController::class, 'store']);
+    Route::put('/universities/{id}', [UniController::class, 'update']);
+    Route::delete('/universities/{id}', [UniController::class, 'destroy']);
+    
+});
+
+
+ 
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
