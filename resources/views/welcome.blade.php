ffcd6ea1bb0191158d1647826949e684a782d5e2
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-04-07 22:20:09 +0100

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-         <!-- Scripts -->
+        
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-    
+        
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-        <title>University Info</title>
-
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,800;1,400;1,500;1,700&display=swap" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,800;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <div class="jubotron">
-            <h1>University Information In Nigeria</h1>
-        </div>
-    </body>
+</head>
+
+<body>
+    <h1>University Infomation</h1>
+</body>
 </html>
