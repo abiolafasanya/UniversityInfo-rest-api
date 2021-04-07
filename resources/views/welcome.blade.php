4a5c5bca34dc5741becc3fce8a84e2ca308e0955
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-04-07 21:41:14 +0100

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,800;1,400;1,500;1,700&display=swap" rel="stylesheet">
     </head>
     <body>
-        
+        <div class="jubotron">
+            <h1>University Information In Nigeria</h1>
+        </div>
     </body>
 </html>
